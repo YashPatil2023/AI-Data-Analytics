5a63dbf0c67fc4a45426ed19c2b228ad4689a0f0
j-a-y-e-s-h <jayeshparmar356890@gmail.com>
2023-08-31 20:22:15 +0530

    update

--- Mutual Fund Dashboard.py
+++ Mutual Fund Dashboard.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import openai
+import time
+
+# Initialize OpenAI API
+openai.api_key = 'Your_Api'  # Replace with your OpenAI API key
 
 # Set the title and introduction
 st.set_page_config(page_title="📈 Mutual Fund Data Analysis", page_icon=":bar_chart:")
@@ -34,18 +39,49 @@ if uploaded_files:
     highest_file_name_gm = file_names[max_gm_index]
     highest_file_name_am = file_names[max_am_index]
 
+
+    # Create an input box for user prompts
+    st.write("## 🤖 Chat with AI")
+    user_prompt = st.text_area("Enter a prompt for analysis:", value="Calculate the trend in the data.")
+    generate_button = st.button("Generate Analysis")
+
+    # Add loading animation when Generate Analysis button is clicked
+    if generate_button:
+        with st.spinner("Generating analysis..."):
+            time.sleep(6)
+        
+        # Combine user prompt with uploaded data information
+        prompt = f"User Prompt: {user_prompt}\n"
+        data_info = ""
+        for file_name, gm, am in zip(file_names, gm_values, am_values):
+            data_info += f"{file_name[:-4]} - Geometric Mean: {gm:.4f}, Arithmetic Mean: {am:.2f}\n"
+        
+        # Combine user prompt and data information for OpenAI
+        combined_prompt = prompt + "Data Information:\n" + data_info
+        
+        # Generate response from OpenAI
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=combined_prompt,
+            max_tokens=100
+        )
+        
+        # Display OpenAI response
+        st.subheader("AI Output")
+        st.write(response.choices[0].text)
+
     # Display the highest means and Adj Close price in colored boxes
     st.sidebar.title("🔝 Top Fund")
 
     highest_name_html = f'<p style="text-align: left; font-size: 26px; color: green; font-weight: bold;">{highest_file_name_gm[:-4]}</p>'
-    st.sidebar.markdown(highest_name_html, unsafe_allow_html=True)  # Remove the ".CSV" extension
+    st.sidebar.markdown(highest_name_html, unsafe_allow_html=True) 
     st.sidebar.markdown(f"**Geometric Mean:** {gm_values[max_gm_index]:.4f}")
     st.sidebar.markdown(f"**Arithmetic Mean:** {am_values[max_gm_index]:.2f}")
 
     highest_price = dfs[max_gm_index]['Adj Close'].max()
     st.sidebar.markdown(f"**Adj Close Price:** ${highest_price:.2f}")
 
-    # Add buttons to display means for all companies
+    # Create buttons to display means for all companies
     display_geometric = st.sidebar.button("📈 Display Geometric Means")
     display_arithmetic = st.sidebar.button("📊 Display Arithmetic Means")
 
@@ -53,28 +89,28 @@ if uploaded_files:
     unique_file_names.sort()
 
     if display_geometric:
-        st.sidebar.markdown("### Geometric Means for All Companies")
+        st.sidebar.subheader("Geometric Means for All Companies")
         for file_name in unique_file_names:
             idx = file_names.index(file_name)
-            st.sidebar.markdown(f"{file_name[:-4]}: Geometric Mean: {gm_values[idx]:.4f}")  # Remove the ".CSV" extension
+            st.sidebar.write(f"{file_name[:-4]}: Geometric Mean: {gm_values[idx]:.4f}")  
 
     if display_arithmetic:
-        st.sidebar.markdown("### Arithmetic Means for All Companies")
+        st.sidebar.subheader("Arithmetic Means for All Companies")
         for file_name in unique_file_names:
             idx = file_names.index(file_name)
-            st.sidebar.markdown(f"{file_name[:-4]}: Arithmetic Mean: {am_values[idx]:.2f}")  # Remove the ".CSV" extension
+            st.sidebar.write(f"{file_name[:-4]}: Arithmetic Mean: {am_values[idx]:.2f}")  
 
     # Create a line chart for Adj Close prices
-    st.write(f"## {highest_file_name_gm[:-4]}")  # Remove the ".CSV" extension
-    fig = px.line(dfs[max_gm_index], x='Date', y='Adj Close', title=f'📈 Adj Close Prices in {highest_file_name_gm[:-4]}')  # Remove the ".CSV" extension
+    st.write(f"## {highest_file_name_gm[:-4]}")  
+    fig = px.line(dfs[max_gm_index], x='Date', y='Adj Close', title=f'📈 Adj Close Prices in {highest_file_name_gm[:-4]}')  
     st.plotly_chart(fig, use_container_width=True)
 
     # Create donut chart for geometric mean of all companies
-    gm_data = pd.DataFrame({'Company': [name[:-4] for name in unique_file_names], 'Geometric Mean': gm_values})  # Remove the ".CSV" extension
+    gm_data = pd.DataFrame({'Company': [name[:-4] for name in unique_file_names], 'Geometric Mean': gm_values})  
     gm_chart = px.pie(gm_data, values='Geometric Mean', names='Company', hole=0.5, title=" Geometric Mean Distribution")
 
     # Create donut chart for arithmetic mean of all companies
-    am_data = pd.DataFrame({'Company': [name[:-4] for name in unique_file_names], 'Arithmetic Mean': am_values})  # Remove the ".CSV" extension
+    am_data = pd.DataFrame({'Company': [name[:-4] for name in unique_file_names], 'Arithmetic Mean': am_values})  
     am_chart = px.pie(am_data, values='Arithmetic Mean', names='Company', hole=0.5, title=" Arithmetic Mean Distribution")
 
     # Display donut charts side by side with space in between
@@ -84,7 +120,7 @@ if uploaded_files:
         st.plotly_chart(gm_chart, use_container_width=True)
 
     with spacer:
-        st.write("")  # Empty space
+        st.write("") 
 
     with col2:
         st.plotly_chart(am_chart, use_container_width=True)
@@ -92,6 +128,6 @@ if uploaded_files:
     # Create bar chart for average Adj Close price of each company
     st.write("## 📊 Average Return for Each Company")
     avg_adj_close_per_company = [np.mean(df['Adj Close']) for df in dfs]
-    avg_per_company_data = pd.DataFrame({'Company': [name[:-4] for name in unique_file_names], 'Average Adj Close Price': avg_adj_close_per_company})  # Remove the ".CSV" extension
+    avg_per_company_data = pd.DataFrame({'Company': [name[:-4] for name in unique_file_names], 'Average Adj Close Price': avg_adj_close_per_company})  
     avg_per_company_chart = px.bar(avg_per_company_data, x='Company', y='Average Adj Close Price', title=" Average Adj Close Price for Each Company")
     st.plotly_chart(avg_per_company_chart, use_container_width=True)
